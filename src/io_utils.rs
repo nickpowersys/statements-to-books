befd4687867113bc832f55d456a3f3175c16fbdc
Nicholas A. Brown <nickpowersys@users.noreply.github.com>
2025-01-02 11:35:48 -0500

    feat: get file path of pdfs

---
+++ src/io_utils.rs
@@ -0,0 +1,22 @@
+use glob::{glob, PatternError};
+use std::path::PathBuf;
+
+pub(crate) fn glob_files_to_process(
+    file_dir: &str,
+    file_ext: &str,
+) -> Result<Vec<PathBuf>, PatternError> {
+    let mut file_paths: Vec<PathBuf> = Vec::new();
+    match glob(&format!("{}/*.{}", file_dir, file_ext)) {
+        Ok(globbed_file_paths) => {
+            for fp in globbed_file_paths {
+                match fp {
+                    Ok(path_buf) => file_paths.push(path_buf),
+                    Err(e) => println!("{:?}", e),
+                }
+            }
+            println!("{:?}", file_paths);
+            Ok(file_paths)
+        }
+        Err(error) => Err(error),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use crate::io_utils::glob_files_to_process;
 use clap::Parser;
+use glob::PatternError;
+use std::path::PathBuf;
+
+pub mod io_utils;
 
 #[derive(Parser)]
 struct Cli {
@@ -10,4 +15,7 @@ fn main() {
     let args = Cli::parse();
     println!("The pdf_dir is {}!", &args.pdf_dir);
     println!("The txt_dir is {}!", &args.txt_dir);
+    let input_file_ext = "txt";
+    let pdf_file_paths: Result<Vec<PathBuf>, PatternError> =
+        Ok(glob_files_to_process(&args.pdf_dir, input_file_ext).unwrap());
 }
