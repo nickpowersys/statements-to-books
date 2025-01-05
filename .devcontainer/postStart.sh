c9af7aee1723ce506e4c97dfdb64719545a6791d
Nicholas A. Brown <nickpowersys@users.noreply.github.com>
2025-01-05 17:26:51 -0500

    build: move LD setup to postStart command; use containerEnv

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -1,3 +1,2 @@
-git config --global --add safe.directory ${containerWorkspaceFolder}
 echo "/root/.local/share/uv/python/cpython-3.11.11-linux-aarch64-gnu/lib" | sudo tee /etc/ld.so.conf.d/uv_python.conf
 sudo ldconfig
