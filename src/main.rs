f67bd839bb923f54fc4b82bf0d34d350d93afede
Nicholas A. Brown <nickpowersys@users.noreply.github.com>
2025-01-06 00:54:36 +0000

    feat: extract text from each page of pdf

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use crate::io_utils::glob_files_to_process;
-use crate::pyo3_pdf_service::get_page_count;
+use crate::pyo3_pdf_service::{extract_text_from_page, get_page_count};
 use clap::Parser;
+use std::ops::Range;
 use std::path::PathBuf;
 
 pub mod io_utils;
@@ -21,6 +22,13 @@ fn main() {
 
     let pdf_file_path: PathBuf = pdf_file_paths[0].clone();
     pyo3::prepare_freethreaded_python();
-    let pg_count = get_page_count(pdf_file_path);
+    let pg_count: u8 = get_page_count(&pdf_file_path).unwrap();
     println!("{:?}", pg_count);
+    let page_range: Range<u8> = 1..pg_count;
+    for page_index in page_range {
+        let pypdf_reader_page_index: u8 = page_index - 1;
+        let pg_text = extract_text_from_page(&pdf_file_path, pypdf_reader_page_index);
+        println!("{}", page_index);
+        println!("{:?}", pg_text);
+    }
 }
--- src/pyo3_pdf_service.rs
+++ src/pyo3_pdf_service.rs
@@ -1,13 +1,28 @@
 use pyo3::prelude::*;
 use std::path::PathBuf;
 
-pub(crate) fn get_page_count(pdf_file_path: PathBuf) -> Result<f64, Box<dyn std::error::Error>> {
+pub(crate) fn get_page_count(pdf_file_path: &PathBuf) -> Result<u8, Box<dyn std::error::Error>> {
     Python::with_gil(|py| {
         let pdf_parser = PyModule::import(py, "statements_to_books.pdf_parser")
             .expect("unable to import 'pdf_parser'");
-        let result: f64 = pdf_parser
+        let result: u8 = pdf_parser
             .getattr("page_count_of_pdf")?
-            .call1((&pdf_file_path,))?
+            .call1((pdf_file_path,))?
+            .extract()?;
+        Ok(result)
+    })
+}
+
+pub(crate) fn extract_text_from_page(
+    pdf_file_path: &PathBuf,
+    page: u8,
+) -> Result<String, Box<dyn std::error::Error>> {
+    Python::with_gil(|py| {
+        let pdf_parser = PyModule::import(py, "statements_to_books.pdf_parser")
+            .expect("unable to import 'pdf_parser'");
+        let result: String = pdf_parser
+            .getattr("extract_text_from_pdf")?
+            .call1((&pdf_file_path, page))?
             .extract()?;
         Ok(result)
     })
